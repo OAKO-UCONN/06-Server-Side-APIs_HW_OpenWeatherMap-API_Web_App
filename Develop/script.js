626df8c7875ff293387dce70ebd434185563326a
OAKO-UCONN <63368794+OAKO-UCONN@users.noreply.github.com>
2020-06-27 23:52:09 -0400

    Initial Commit
    @master Branch

---
+++ Develop/script.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+  $("#search-button").on("click", function () {
+    var searchValue = $("#search-value").val();
+
+    // clear input box
+    $("#search-value").val("");
+    //Search Value is the City name
+    searchWeather(searchValue);
+  });
+
+  function searchWeather(searchValue) {
+    $.ajax({
+      type: "GET",
+      url:
+        "http://api.openweathermap.org/data/2.5/weather?q=" +
+        searchValue +
+        "&appid=" +
+        /* your API key here */ +"&units=imperial",
+      dataType: "json",
+      success: function (data) {
+        //Data is weather info back from the API
+      }
+    });
+  }
+});
